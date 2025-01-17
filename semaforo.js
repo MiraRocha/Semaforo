efdbc3aecf69dbd52cb3dc86b502fd1bd689f976
MiraRocha <MiraRocha@users.noreply.github.com>
2025-01-17 18:09:44 +0000

    +

--- semaforo.js
+++ semaforo.js
@@ -5,10 +5,17 @@ const trafficLight = (event) => {
     turnOn[event.target.id]();
 }
 
+
+const changecolor = () => {
+    const colors = ['red', 'yellow', 'green'];
+    const color = 'red';
+    turnOn[color]();
+}
 const turnOn = {
     'red': () => img.src = './img/vermelho.png',
     'yellow': () => img.src = './img/amarelo.png',
-    'green': () => img.src = './img/verde.png'
+    'green': () => img.src = './img/verde.png',
+    'automatic': () => setInterval(changecolor, 1000)
 
 }
 
